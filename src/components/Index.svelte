4209be44f68dab07e002883040a32d4328ae1fc9
Toms Valdāts <104590019+tomsval@users.noreply.github.com>
2023-09-25 17:05:13 +0000

    update index css

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,1 +1,16 @@
-<h1>valdats.id.lv</h1>
+
+<div class="index-component">
+    <h1>valdats.id.lv</h1>
+</div>
+
+<style>
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .index-component {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
